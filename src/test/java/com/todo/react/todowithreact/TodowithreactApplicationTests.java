90aedc65146037b16064ff86306accbc5c6ab252
Anand Mohan Sharma <puraskaarwebteam@gmail.com>
2024-02-02 23:39:22 +0530

    TODO REST API initiated

---
+++ src/test/java/com/todo/react/todowithreact/TodowithreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.react.todowithreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodowithreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
